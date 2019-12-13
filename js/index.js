97aba41f2d5262c50725c4420d8c48dc4076cc05
JeanOryon <48481925+JeanOryon@users.noreply.github.com>
2019-12-13 12:38:50 +0100

    change
    change

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+function Unpeu () {
+    document.getElementById("cache").innerHTML=""
+    document.getElementById("").innerHTML = "cache";
+}
+
+function Unpeu() {
+    newcache = "cache";
+    document.getElementById("cache").innerHTML = newcache;
+  }
+
+function Beaucoup () {
+
+}
+
+function Enorme () {
+
+}
